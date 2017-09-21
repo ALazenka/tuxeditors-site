f6cafc1714edc5a390828ff260ee96fb23e36744
ALazenka <andrewlazenka@gmail.com>
2017-09-21 15:14:40 -0400

    changed site to create-react-app

--- site/app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-class HomeController < ApplicationController
-  require 'net/http'
-  require 'uri'
-
-  def index
-    req = HTTP.get('https://api.github.com/repos/ALazenka/tuxeditors-site/commits')
-    @commit_list = JSON.parse(req.body)
-  end
-end
