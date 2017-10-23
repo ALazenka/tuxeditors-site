dd68a95642767be2f7f18914a98b528c99ea73f9
ALazenka <andrewlazenka@gmail.com>
2017-10-23 01:38:55 -0400

    Added conceptual architecture report link, updated discussions page

--- tuxeditors-site/src/ConceptualArchitecture.js
+++ tuxeditors-site/src/ConceptualArchitecture.js
@@ -4,13 +4,14 @@ class ConceptualArchitecture extends Component {
   render() {
     return (
       <center>
-        <h3><a href="https://docs.google.com/presentation/d/1j-BdI9GyhXWbmU_H5l3i17oJaizkwN3QPSMOGeIrAHQ/edit?usp=sharing">Conceptual Architecture</a></h3>
+        <h3>Conceptual Architecture</h3>
+        <h5><a href="https://docs.google.com/presentation/d/1j-BdI9GyhXWbmU_H5l3i17oJaizkwN3QPSMOGeIrAHQ/edit?usp=sharing">Presentation Slides</a> | <a href="https://docs.google.com/document/d/1RR-ftnZRks9C3IgAzHQvd173un1lKESIocdQe5VHhMw/edit">Report</a></h5>
         <iframe 
           title="TuxeditorsConceptualArchitecture"
           src="https://docs.google.com/presentation/d/e/2PACX-1vQE4UprHUNMn9KepvH2fTq4kyRSJXepcLXsvq8CIhQ1VKFBUT6jM9A3k5wTcTtPE4L7Eh8LzdcxOhkI/embed?start=false&loop=true&delayms=3000"
           frameBorder="0"
-          width="1440"
-          height="839"
+          width="480"
+          height="299"
           allowFullScreen="true"
         />
       </center>
--- tuxeditors-site/src/Discussions.js
+++ tuxeditors-site/src/Discussions.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import { Well } from 'react-bootstrap';
 
 class Discussions extends Component {
-    render() {
-        return (
-            <center>
-              <h3 id="discussions">Discussions</h3>
-              <h4>Meeting #1 - Sept 28:</h4>
-              <p>Discussed website design and layout. Git and version control was taught to unfamiliar members.</p>
-            </center>
-        );
-    }
+  render() {
+    return (
+      <center>
+        <h3 id="discussions">Discussions</h3>
+        <h4>Meeting #1 - Sept 28:</h4>
+        <p>Discussed website design and layout. Git and version control was taught to unfamiliar members.</p>
+        <h4>Meeting #2 - October 4:</h4>
+        <p>Created architecture diagram what we hypthothesized.</p>
+        <h4>Meeting #3 - October 12:</h4>
+        <p>Assigned which part of report/presentation each team member is responsible for.</p>
+        <h4>Meeting #4 - October 15:</h4>
+        <p>Shared assigned work, and consolidated knowledge and feedback. Created presentation slides.</p>
+        <h4>Meeting #5 - October 18:</h4>
+        <p>Practiced presentation and gave each other feedback.</p>
+        <h4>Meeting #6 - October 22:</h4>
+        <p>Congregated inidividual report sections, and proofread each others’ work.</p>
+      </center>
+    );
+  }
 }
 
 
